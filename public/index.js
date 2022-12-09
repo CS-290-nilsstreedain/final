5e6c0c80baf5aca6095d96e212b474d8204e2694
awhittle2 <whittleabigail@gmail.com>
2022-12-09 08:51:04 -0800

    Added store functionality

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var wood = 0 // User wood total
+var wood = 1000000000 // User wood total
 var wpc = 1 // Wood per click
 var wps = 0 // Wood per second
 const fps = 30 // FPS
@@ -48,6 +48,70 @@ chainsaw.addEventListener('click', () => purchase('chainsaw', 'Chainsaw', '75', 
 autoChainsaw.addEventListener('click', () => purchase('autoChainsaw', 'Auto Chainsaw', '150', 0, 0, 3));
 flameThrower.addEventListener('click', () => purchase('flameThrower', 'Flame Thrower', '125', 0, 5, 0));
 autoFlameThrower.addEventListener('click', () => purchase('autoFlameThrower', 'Auto Flame Thrower', '375', 0, 0, 5));
+
+storePosts = document.getElementById('store')
+
+
+var alreadyWon = false
+
+storePosts.addEventListener('click', function(event) {
+	if(event.target.parentNode.classList.contains("post") && !event.target.parentNode.classList.contains("bought")) {
+		var post = event.target.parentNode
+		var price = post.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.textContent
+		price = price.substr(1, price.length)
+		priceNum = Number(price)
+		if(wood >= priceNum) {
+			wood -= priceNum
+			post.classList.add("bought")
+		} else {
+			alert("You need " + priceNum + " wood to purchase this item.")
+		}
+	}
+	else if (event.target.parentNode.parentNode.classList.contains("post") && !event.target.parentNode.parentNode.classList.contains("bought")) {
+		var post = event.target.parentNode.parentNode
+		var price = post.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.textContent
+		price = price.substr(1, price.length)
+		priceNum = Number(price)
+		if(wood >= priceNum) {
+			wood -= priceNum
+			post.classList.add("bought")
+		} else {
+			alert("You need " + priceNum + " wood to purchase this item.")
+		}
+	}
+	else if (event.target.parentNode.parentNode.parentNode.classList.contains("post") && !event.target.parentNode.parentNode.parentNode.classList.contains("bought")) {
+		var post = event.target.parentNode.parentNode.parentNode
+		var price = post.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.nextSibling.nextSibling.textContent
+		price = price.substr(1, price.length)
+		priceNum = Number(price)
+		if(wood >= priceNum) {
+			wood -= priceNum
+			post.classList.add("bought")
+		} else {
+			alert("You need " + priceNum + " wood to purchase this item.")
+		}
+	}
+
+	
+	var boughtItems = 0
+	var postIndex = 0
+	var postList = document.getElementsByClassName("post")
+	
+	while(postIndex < postList.length) {
+		if(postList[postIndex].classList.contains("bought")) {
+			boughtItems++
+		}
+		postIndex++
+	}
+
+	postIndex = 0
+
+	if(boughtItems === (postList.length) && !alreadyWon) {
+		alert("Congrats! You have bought Benny's entire List!!! You win!!!!")
+		alreadyWon = true
+	}
+})
+
 
 // Normal click
 woodButton.addEventListener('click', function() {

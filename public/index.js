2602ff069ebf0d3af93fe30ec20afdcb1faa541e
Nils Streedain <nils@streedain.com>
2022-12-09 11:11:21 -0800

    Fixed buttons not updating price/count

--- public/index.js
+++ public/index.js
@@ -1,20 +1,28 @@
-var wood = 0 // User wood total
-var wpc = 1 // Wood per click
-var wps = 0 // Wood per second
-const fps = 30 // FPS
+var wood = 0;	// User wood total
+var wpc = 1;	// Wood per click
+var wps = 0;	// Wood per second
+const fps = 30;	// FPS
 
-// Generalized function for pruchases
-function purchase(vname, name, price, count, wpcs, wpss) {
-	if (wood >= price) {
-		wpc += wpcs
-		wps += wpss
-		wood -= price
-		price = Math.round(price * 1.25)
-		count += 1
-		document.getElementById(vname + 'Title').textContent = "Buy " + name + " - " + price
-		document.getElementById(vname + 'Counter').textContent = name + "s: " + count
+function Tool(vname, name, price, count, wpcs, wpss) {
+	this.vname = vname;
+	this.name = name;
+	this.price = price;
+	this.count = count;
+	this.wpcs = wpcs;
+	this.wpss = wpss;
+}
+
+function purchase(tool) {
+	if (wood >= tool.price) {
+		wpc += tool.wpcs;
+		wps += tool.wpss;
+		wood -= tool.price;
+		tool.price = Math.round(tool.price * 1.25);
+		tool.count++;
+		document.getElementById(tool.vname + 'Title').textContent = `Buy ${tool.name} - ${tool.price}`;
+		document.getElementById(tool.vname + 'Counter').textContent = `${tool.name}s: ${tool.count}`;
 	} else {
-		alert("You need " + price + " wood to purchase this item.")
+		alert(`You need ${tool.price} wood to purchase this item.`);
 	}
 }
 
@@ -22,8 +30,8 @@ function endGame() {
 	fetch('/getMinScore')
 	.then(response => response.json())
 	.then(json => {
-		var name = ""
-		if ((json.count < 10 || wood > json.score) && (name = prompt("Congratulations, you've scored in the top 10! Please enter your name to be added to the leaderboard:"))) {
+		var name = ''
+		if ((json.count < 10 || wood > json.score) && (name = prompt('Congratulations, you\'ve scored in the top 10! Please enter your name to be added to the leaderboard:'))) {
 			fetch('/addScore', {
 				method: 'POST',
 				body: JSON.stringify({
@@ -34,30 +42,40 @@ function endGame() {
 				headers: {
 					'Content-Type': 'application/json'
 				}
-			})
+			});
 		}
 	}).then(function(res) {
 		window.location = '/leaderboard';
 	});
 }
 
+// Array of Tool objects
+var tools = [
+	new Tool('axe', 'Axe', '25', 0, 1, 0),
+	new Tool('autoAxe', 'Auto Axe', '50', 0, 0, 1),
+	new Tool('chainsaw', 'Chainsaw', '75', 0, 3, 0),
+	new Tool('autoChainsaw', 'Auto Chainsaw', '150', 0, 0, 3),
+	new Tool('flameThrower', 'Flame Thrower', '125', 0, 5, 0),
+	new Tool('autoFlameThrower', 'Auto Flame Thrower', '375', 0, 0, 5)
+];
+
 // Event listeners for purchases
-axe.addEventListener('click', () => purchase('axe', 'Axe', '25', 0, 1, 0));
-autoAxe.addEventListener('click', () => purchase('autoAxe', 'Auto Axe', '50', 0, 0, 1));
-chainsaw.addEventListener('click', () => purchase('chainsaw', 'Chainsaw', '75', 0, 3, 0));
-autoChainsaw.addEventListener('click', () => purchase('autoChainsaw', 'Auto Chainsaw', '150', 0, 0, 3));
-flameThrower.addEventListener('click', () => purchase('flameThrower', 'Flame Thrower', '125', 0, 5, 0));
-autoFlameThrower.addEventListener('click', () => purchase('autoFlameThrower', 'Auto Flame Thrower', '375', 0, 0, 5));
+axe.addEventListener('click', () => purchase(tools[0]));
+autoAxe.addEventListener('click', () => purchase(tools[1]));
+chainsaw.addEventListener('click', () => purchase(tools[2]));
+autoChainsaw.addEventListener('click', () => purchase(tools[3]));
+flameThrower.addEventListener('click', () => purchase(tools[4]));
+autoFlameThrower.addEventListener('click', () => purchase(tools[5]));
 
 // Normal click
 woodButton.addEventListener('click', function() {
-	wood += wpc
+	wood += wpc;
 });
 
 // Warn when selecting leaderboard
 leaderboard.onclick = function() {
 	if (wood != 0) {
-		if (confirm("Wait! Leaving this page will end the game! Are you sure?")) {
+		if (confirm('Wait! Leaving this page will end the game! Are you sure?')) {
 			endGame();
 		}
 		return false;
@@ -66,6 +84,6 @@ leaderboard.onclick = function() {
 
 setInterval(function() {
     wood += wps/fps;
-	woodTitle.textContent = "Wood Count: " + Math.round(wood);
-	woodPer.textContent = "Wood Per Sec: " + Math.round(wps);
-}, 1000/fps);
+	woodTitle.textContent = `Wood Count: ${Math.round(wood)}`;
+	woodPer.textContent = `Wood/Sec: ${Math.round(wps)}`;
+}, 1000 / fps);

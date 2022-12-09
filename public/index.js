cfb3eeb8aabe14fba5a4721f6edb698eddaa9939
Nils Streedain <nils@streedain.com>
2022-12-08 22:37:21 -0800

    Added endgame and saving to leaderboard
    Co-authored-by: Steen Olson <olsostee@oregonstate.edu>

--- public/index.js
+++ public/index.js
@@ -3,24 +3,35 @@ var wpc = 1 // Wood per click
 var wps = 0 // Wood per second
 const fps = 30 // FPS
 
-// Exponential increase
-function increaseVar(temp) {
-    return Math.round(temp * 1.25)
-}
-
 // Generalized function for pruchases
 function purchase(vname, name, price, count, wpcs, wpss) {
 	if (wood >= price) {
 		wpc += wpcs
 		wps += wpss
 		wood -= price
-		price = increaseVar(price)
+		price = Math.round(price * 1.25)
 		count += 1
 		document.getElementById(vname + 'Title').textContent = "Buy " + name + " - " + price
 		document.getElementById(vname + 'Counter').textContent = name + "s: " + count
 	} else {
 		alert("You need " + price + " wood to purchase this item.")
 	}
+}
+
+function endGame() {
+	fetch('/addScore', {
+		method: 'POST',
+		body: JSON.stringify({
+			name: prompt("Please enter your name:"),
+			score: wood,
+			date: new Date(Date.now()).toLocaleDateString()
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(function(res) {
+		console.log(res.status)
+	})
 }
 
 // Event listeners for purchases
@@ -39,6 +50,7 @@ woodButton.addEventListener('click', function() {
 // Warn when selecting leaderboard
 leaderboard.onclick = function() {
     if (confirm("Wait! Leaving this page will end the game! Are you sure?")){
+		endGame();
 	} else {
 		return false;
 	}
